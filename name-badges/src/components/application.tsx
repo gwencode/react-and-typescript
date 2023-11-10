6b6f1e743508da219008707d1c2be7ba9f5955da
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-10 13:29:05 +0100

    Lesson: React Component with TypeScript

---
+++ name-badges/src/components/application.tsx
@@ -0,0 +1,10 @@
+// import ControlPanel from './controls';
+import NameBadge from './name-badge';
+
+const Application = () => (
+  <main className="application">
+    <NameBadge name="Gwendal LE BRIS" />
+  </main>
+);
+
+export default Application;
