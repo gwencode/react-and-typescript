88d38f62e73db983ba27dced72afd3b176dbae2f
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-13 17:47:47 +0100

    Typing Reducer of AccidentCounter

--- 03-accident-counter/src/components/counter.tsx
+++ 03-accident-counter/src/components/counter.tsx
@@ -1,24 +1,65 @@
-import { useState } from 'react';
+import { useReducer, useState } from 'react';
+
+const intialState = {
+  count: 0,
+  draftCount: 0,
+};
+
+type ActionType = 'increment' | 'decrement' | 'reset' | 'update' | 'change';
+
+const reducer = (
+  state = intialState,
+  action: {
+    type: ActionType;
+    payload?: number;
+  },
+) => {
+  const { count, draftCount } = state;
+  if (action.type === 'increment') {
+    return { count: count + 1, draftCount };
+  }
+
+  if (action.type === 'decrement') {
+    return { count: count - 1, draftCount };
+  }
+
+  if (action.type === 'reset') {
+    return { count: 0, draftCount };
+  }
+
+  if (action.type === 'update') {
+    const newCount = draftCount;
+    return { count: newCount, draftCount: 0 };
+  }
+
+  if (action.type === 'change' && action.payload) {
+    const newDraftCount = action.payload;
+    return { count, draftCount: newDraftCount };
+  }
+
+  return state;
+};
 
 const Counter = (): JSX.Element => {
-  const [count, setCount] = useState(0);
-  const [inputCount, setInputCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  // const [inputCount, setInputCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, intialState);
 
-  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-    // console.log(e.target.value);
-    setInputCount(e.target.valueAsNumber);
-  };
+  // const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   // console.log(e.target.value);
+  //   setInputCount(e.target.valueAsNumber);
+  // };
 
   return (
     <section className="flex w-2/3 flex-col items-center gap-8 border-4 border-primary-500 bg-white p-8 shadow-lg">
       <h1>Days Since the Last Accident</h1>
-      <p className="text-6xl">{count}</p>
+      <p className="text-6xl">{state.count}</p>
       <div className="flex gap-2">
-        <button onClick={() => setCount((count) => count - 1)}>
+        <button onClick={() => dispatch({ type: 'decrement' })}>
           ➖ Decrement
         </button>
-        <button onClick={() => setCount(0)}>🔁 Reset</button>
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => dispatch({ type: 'reset' })}>🔁 Reset</button>
+        <button onClick={() => dispatch({ type: 'increment' })}>
           ➕ Increment
         </button>
       </div>
@@ -26,11 +67,16 @@ const Counter = (): JSX.Element => {
         <form
           onSubmit={(e) => {
             e.preventDefault();
-            setCount(inputCount);
-            setInputCount(0);
+            dispatch({ type: 'update' });
           }}
         >
-          <input type="number" value={inputCount} onChange={handleInput} />
+          <input
+            type="number"
+            value={state.draftCount}
+            onChange={(e) =>
+              dispatch({ type: 'change', payload: e.target.valueAsNumber })
+            }
+          />
           <button type="submit">Update Counter</button>
         </form>
       </div>
