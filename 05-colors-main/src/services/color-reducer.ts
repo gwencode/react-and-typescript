fdc8a1f6ad86c192e852b7197bf56e00104ac905
Gwendal Le Bris <gwen.coding@gmail.com>
2023-11-14 14:58:23 +0100

    Creating Actions for colorReducer

---
+++ 05-colors-main/src/services/color-reducer.ts
@@ -0,0 +1,47 @@
+import { rgb } from 'color-convert';
+
+type colorState = {
+  hexColor: string
+}
+
+export const initialState: colorState = {
+  hexColor: "#BADA55"
+}
+
+type UpdateHexColor = {
+  type: 'update-hex-color';
+  payload: {hexColor: string}
+}
+
+type UpdateRGBColor = {
+  type: 'update-rgb-color';
+  payload: { rgb : [number, number, number]}
+}
+
+export const colorReducer = (
+  state: colorState = initialState,
+  action: UpdateHexColor | UpdateRGBColor
+  ): colorState => {
+    switch (action.type) {
+      case 'update-hex-color': {
+        const { hexColor } = action.payload;
+        return {...state, hexColor };
+      }
+      case 'update-rgb-color': {
+        const hexColor = rgb.hex(action.payload.rgb);
+        return {...state, hexColor };
+      }
+      default:
+        return state;
+    }
+  }
+
+// if (action.type === 'update-hex-color') {
+//   const { hexColor } = action.payload;
+//   return {...state, hexColor };
+// }
+
+// if (action.type === 'update-rgb-color') {
+//   const hexColor = rgb.hex(action.payload.rgb)
+//   return {...state, hexColor };
+// }
